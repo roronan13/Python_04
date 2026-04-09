dc8afbef103217f397c78d9ca80e773989690550
roronan13 <ronan.pothier@gmail.com>
2026-04-09 18:34:18 +0200

    allez

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage : python3 ft_ancient_text.py <file>")
+        sys.exit()
+
+    print(" === Cyber Archives Recovery === \n")
+
+    print(f"Accessing file '{sys.argv[1]}'")
+    try:
+        file = open(sys.argv[1], "r")
+    except FileNotFoundError as e:
+        print(f"Error opening file '{sys.argv[1]}' : {e}")
+        sys.exit()
+    except PermissionError as e:
+        print(f"Error opening file '{sys.argv[1]}' : {e}")
+        sys.exit()
+
+    print(" --- ")
+    text: str = file.read()
+    print(text)
+    print(" --- ")
+
+    file.close()
+    print(f"File '{sys.argv[1]}' closed.")
+
+    file = open(sys.argv[1], "rw")
+    old_text: str = file.read()
+
+    old_lines: list[str] = old_text.split('\n')
+    for old_line in old_lines:
+        
