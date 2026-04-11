4e5f505321f4598e2072ea52cb8f86e386c1a791
roronan13 <ronan.pothier@gmail.com>
2026-04-11 18:49:06 +0200

    dacc

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -31,12 +31,33 @@ if __name__ == "__main__":
     file = open(sys.argv[1], "r")
     old_text: str = file.read()
 
-    print("Transform data : ")
-    print()
+    print("\nTransform data : ")
+    print(" --- ")
     new_text: str = ""
     old_lines: list[str] = old_text.split('\n')
     for old_line in old_lines:
         new_line = old_line + "#\n"
         new_text += new_line
-    
-    print(new_text)
+
+    print(new_text, end="")
+    print(" --- ")
+
+    new_file_name: str = input("\nEnter new file name to save transformed \
+data (empty won't save anything)\n")
+    if new_file_name == "":
+        print("Not saving data\n")
+        sys.exit()
+    else:
+        print(f"\nSaving data to '{new_file_name}'")
+        try:
+            new_file = open(new_file_name, "w")
+            new_file.write(new_text)
+            print(f"Data saved in '{new_file_name}'\n")
+        except Exception as e:
+            print(f"Error creating or writing new file : {e}\n")
+
+    new_file = open(new_file_name, "r")
+    text_in_new_file: str = new_file.read()
+    print(text_in_new_file)
+    new_file.close()
+    print(f"File '{new_file_name}' closed.")
